2edb645d758c8aadb5c0a9051e3d789cdaa45b8c
1127071993@qq.com <aini2009626>
2019-11-12 21:59:07 +0800

    3-2 字体图标准备

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <!-- <div id="read"></div> -->
+    <span class="icon-bookmark"></span>
+  </div>
+</template>
+
+<script>
+import Epub from 'epubjs'
+global.ePub = Epub
+export default {
+  name: 'home',
+  mounted () {
+    this.book = new Epub('/2018_Book_AgileProcessesInSoftwareEngine.epub')
+    console.log(this.book)
+    this.book.renderTo('read', {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }).display()
+  }
+}
+</script>
