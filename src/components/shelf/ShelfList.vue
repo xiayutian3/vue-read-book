dbfa7a4a2a6cd559e5fab073d3f71e5e46a5046a
1127071993@qq.com <aini2009626>
2020-01-01 17:16:29 +0800

    shelf/ShelfList.vue  的key值问题

--- src/components/shelf/ShelfList.vue
+++ src/components/shelf/ShelfList.vue
@@ -6,7 +6,7 @@
       给需移动位置的元素添加 :.list-move  class
       -->
     <transition-group name="list" tag="div" id="shelf-list">
-      <div class="shelf-list-item-wrapper" v-for="item in data" :key="item.id">
+      <div class="shelf-list-item-wrapper" v-for="(item,index) in data" :key="index">
         <shelf-item :data="item" :style="{height:itemHeight}"></shelf-item>
         <div class="shelf-list-title-wrapper">
           <span class="shelf-list-title title-small">{{item.title}}</span>
